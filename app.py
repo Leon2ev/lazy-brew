ce545201ac06c24a654cc1364c3d732dd522d90e
Ubuntu <ubuntu@ip-172-31-36-202.ec2.internal>
2019-08-15 10:46:20 +0000

    Base.html template added

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# Connection to MongoDB
+
 app.config["MONGO_DBNAME"] = 'beer_brewing'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
+
+# App routes
 
 @app.route('/')
 @app.route('/get_products')
