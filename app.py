5a3ece529e29b4836285097a5580fc554854abda
Ubuntu <ubuntu@ip-172-31-36-202.ec2.internal>
2019-08-27 14:26:37 +0000

    Added add_product form and discription page structure changed

--- app.py
+++ app.py
@@ -17,11 +17,26 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_products')
 def get_products():
-    return render_template("products.html", products=mongo.db.products.find())
+    return render_template("products.html", 
+    products=mongo.db.products.find())
+
+@app.route('/add_product')
+def add_product():
+    return render_template("addproduct.html",
+    brands=mongo.db.brands.find(),
+    methods=mongo.db.methods.find(),
+    types=mongo.db.types.find())
+    
+@app.route('/insert_product', methods=["POST"])
+def insert_product():
+    products=mongo.db.products
+    products.insert_one(request.form.to_dict())
+    return redirect(url_for('get_products'))
     
 @app.route('/get_description')
 def get_description():
-    return render_template("description.html", products=mongo.db.products.find())
+    return render_template("description.html", 
+    products=mongo.db.products.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
