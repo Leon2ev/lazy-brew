2232e5d7e0514cb08323c7fe835d982026294c05
Ubuntu <ubuntu@ip-172-31-36-202.ec2.internal>
2019-08-29 18:47:17 +0000

    Added comments

--- app.py
+++ app.py
@@ -12,25 +12,34 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
-# App routes
+# App routes ------------------------------------------------------------
 
 @app.route('/')
+
+# Product selection
+
 @app.route('/get_products')
 def get_products():
     return render_template("products.html", 
     products=mongo.db.products.find())
+
+# Add product
 
 @app.route('/add_product')
 def add_product():
     return render_template("addproduct.html",
     brands=mongo.db.brands.find(),
     types=mongo.db.types.find())
+
+# Proceed inserted data to mongo
     
 @app.route('/insert_product', methods=["POST"])
 def insert_product():
     products=mongo.db.products
     products.insert_one(request.form.to_dict())
     return redirect(url_for('get_products'))
+
+# Single product description
     
 @app.route('/get_description/<product_id>')
 def get_description(product_id):
