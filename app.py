510e150dd1ea587af76e9dcc811c0abcd4c55693
Ubuntu <ubuntu@ip-172-31-36-202.ec2.internal>
2019-09-10 09:27:26 +0000

    mongoengine removed

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from mongoengine import connect
 
 app = Flask(__name__)
 
